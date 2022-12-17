22123f12c11627651a5f5bfeb88b00cdf738dbbf
XxRexRaptorxX <Raptor-Simon@web.de>
2022-12-17 12:42:15 +0100

    port to 1.19.3

--- src/main/java/xxrexraptorxx/builderstabs/main/package-info.java
+++ src/main/java/xxrexraptorxx/builderstabs/main/package-info.java
@@ -2,4 +2,4 @@
  * @author XxRexRaptorxX (RexRaptor)
  * @projectPage https://www.curseforge.com/minecraft/mc-mods/builders-tabs
  **/
-package xxrexraptorxx.main;
+package xxrexraptorxx.builderstabs.main;

40c7f47f73408c8a42796b7b7e65a711d669bbf4
XxRexRaptorxX <Raptor-Simon@web.de>
2025-10-22 21:19:14 +0200

    style: code formatting

--- src/main/java/xxrexraptorxx/builderstabs/main/CreativeTab.java
+++ src/main/java/xxrexraptorxx/builderstabs/main/CreativeTab.java
@@ -1,16 +1,10 @@
 package xxrexraptorxx.builderstabs.main;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.properties.PropertyMap;
-import com.mojang.authlib.yggdrasil.ProfileResult;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -24,62 +18,63 @@ import java.util.UUID;
 
 public class CreativeTab {
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BuildersTabs.MODID) ;
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BuildersTabs.MODID);
 
-    public static void init(IEventBus bus) { CREATIVE_MODE_TABS.register(bus); }
+    public static void init(IEventBus bus) {
+        CREATIVE_MODE_TABS.register(bus);
+    }
 
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN_TAB = CREATIVE_MODE_TABS.register(BuildersTabs.MODID, () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup." + BuildersTabs.MODID + "_tab"))
-            .icon(() -> createNBTItemStack(Minecraft.getInstance().player.getName().getString()))
-            .displayItems((params, output) -> {
-                output.accept(createNBTItemStack("XxRexRaptorxX"));
-                output.accept(createNBTItemStack(Minecraft.getInstance().player.getName().getString()));
-                output.accept(createNBTItemStack("MHF_Steve"));
-                output.accept(createNBTItemStack("MHF_Alex"));
-                output.accept(createNBTItemStack("MHF_Blaze"));
-                output.accept(createNBTItemStack("MHF_CaveSpider"));
-                output.accept(createNBTItemStack("MHF_Chicken"));
-                output.accept(createNBTItemStack("MHF_Cow"));
-                output.accept(createNBTItemStack("MHF_Creeper"));
-                output.accept(createNBTItemStack("MHF_Enderman"));
-                output.accept(createNBTItemStack("MHF_Ghast"));
-                output.accept(createNBTItemStack("MHF_Golem"));
-                output.accept(createNBTItemStack("MHF_Herobrine"));
-                output.accept(createNBTItemStack("MHF_LavaSlime"));
-                output.accept(createNBTItemStack("MHF_MushroomCow"));
-                output.accept(createNBTItemStack("MHF_Ocelot"));
-                output.accept(createNBTItemStack("MHF_Pig"));
-                output.accept(createNBTItemStack("MHF_PigZombie"));
-                output.accept(createNBTItemStack("MHF_Sheep"));
-                output.accept(createNBTItemStack("MHF_Skeleton"));
-                output.accept(createNBTItemStack("MHF_Slime"));
-                output.accept(createNBTItemStack("MHF_Spider"));
-                output.accept(createNBTItemStack("MHF_Squid"));
-                output.accept(createNBTItemStack("MHF_Villager"));
-                output.accept(createNBTItemStack("MHF_WSkeleton"));
-                output.accept(createNBTItemStack("MHF_Zombie"));
-                output.accept(createNBTItemStack("MHF_Cactus"));
-                output.accept(createNBTItemStack("MHF_Cake"));
-                output.accept(createNBTItemStack("MHF_Chest"));
-                output.accept(createNBTItemStack("MHF_CoconutB"));
-                output.accept(createNBTItemStack("MHF_CoconutG"));
-                output.accept(createNBTItemStack("MHF_Melon"));
-                output.accept(createNBTItemStack("MHF_OakLog"));
-                output.accept(createNBTItemStack("MHF_Present1"));
-                output.accept(createNBTItemStack("MHF_Present2"));
-                output.accept(createNBTItemStack("MHF_Pumpkin"));
-                output.accept(createNBTItemStack("MHF_TNT"));
-                output.accept(createNBTItemStack("MHF_TNT2"));
-                output.accept(createNBTItemStack("MHF_ArrowUp"));
-                output.accept(createNBTItemStack("MHF_ArrowDown"));
-                output.accept(createNBTItemStack("MHF_ArrowLeft"));
-                output.accept(createNBTItemStack("MHF_ArrowRight"));
-                output.accept(createNBTItemStack("MHF_Exclamation"));
-                output.accept(createNBTItemStack("MHF_Question"));
-            }).build());
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN_TAB = CREATIVE_MODE_TABS.register(BuildersTabs.MODID,
+            () -> CreativeModeTab.builder().title(Component.translatable("itemGroup." + BuildersTabs.MODID + "_tab"))
+                    .icon(() -> createNBTItemStack(Minecraft.getInstance().player.getName().getString())).displayItems((params, output) -> {
+                        output.accept(createNBTItemStack("XxRexRaptorxX"));
+                        output.accept(createNBTItemStack(Minecraft.getInstance().player.getName().getString()));
+                        output.accept(createNBTItemStack("MHF_Steve"));
+                        output.accept(createNBTItemStack("MHF_Alex"));
+                        output.accept(createNBTItemStack("MHF_Blaze"));
+                        output.accept(createNBTItemStack("MHF_CaveSpider"));
+                        output.accept(createNBTItemStack("MHF_Chicken"));
+                        output.accept(createNBTItemStack("MHF_Cow"));
+                        output.accept(createNBTItemStack("MHF_Creeper"));
+                        output.accept(createNBTItemStack("MHF_Enderman"));
+                        output.accept(createNBTItemStack("MHF_Ghast"));
+                        output.accept(createNBTItemStack("MHF_Golem"));
+                        output.accept(createNBTItemStack("MHF_Herobrine"));
+                        output.accept(createNBTItemStack("MHF_LavaSlime"));
+                        output.accept(createNBTItemStack("MHF_MushroomCow"));
+                        output.accept(createNBTItemStack("MHF_Ocelot"));
+                        output.accept(createNBTItemStack("MHF_Pig"));
+                        output.accept(createNBTItemStack("MHF_PigZombie"));
+                        output.accept(createNBTItemStack("MHF_Sheep"));
+                        output.accept(createNBTItemStack("MHF_Skeleton"));
+                        output.accept(createNBTItemStack("MHF_Slime"));
+                        output.accept(createNBTItemStack("MHF_Spider"));
+                        output.accept(createNBTItemStack("MHF_Squid"));
+                        output.accept(createNBTItemStack("MHF_Villager"));
+                        output.accept(createNBTItemStack("MHF_WSkeleton"));
+                        output.accept(createNBTItemStack("MHF_Zombie"));
+                        output.accept(createNBTItemStack("MHF_Cactus"));
+                        output.accept(createNBTItemStack("MHF_Cake"));
+                        output.accept(createNBTItemStack("MHF_Chest"));
+                        output.accept(createNBTItemStack("MHF_CoconutB"));
+                        output.accept(createNBTItemStack("MHF_CoconutG"));
+                        output.accept(createNBTItemStack("MHF_Melon"));
+                        output.accept(createNBTItemStack("MHF_OakLog"));
+                        output.accept(createNBTItemStack("MHF_Present1"));
+                        output.accept(createNBTItemStack("MHF_Present2"));
+                        output.accept(createNBTItemStack("MHF_Pumpkin"));
+                        output.accept(createNBTItemStack("MHF_TNT"));
+                        output.accept(createNBTItemStack("MHF_TNT2"));
+                        output.accept(createNBTItemStack("MHF_ArrowUp"));
+                        output.accept(createNBTItemStack("MHF_ArrowDown"));
+                        output.accept(createNBTItemStack("MHF_ArrowLeft"));
+                        output.accept(createNBTItemStack("MHF_ArrowRight"));
+                        output.accept(createNBTItemStack("MHF_Exclamation"));
+                        output.accept(createNBTItemStack("MHF_Question"));
+                    }).build());
 
-    //TODO FIX!
+    // TODO FIX!
     private static ItemStack createNBTItemStack(String name) {
         ItemStack stack = new ItemStack(Items.PLAYER_HEAD);
         stack.set(DataComponents.PROFILE, new ResolvableProfile(Optional.of(name), Optional.empty(), new PropertyMap()));
